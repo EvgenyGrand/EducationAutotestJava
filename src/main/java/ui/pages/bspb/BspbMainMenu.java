5154e1b2455f4ee250f548a2f7ee6fff70631fbe
echis <Evgeny.Grand@mail.ru>
2023-04-29 22:19:35 +0300

    run mvn install

--- src/main/java/ui/pages/bspb/BspbMainMenu.java
+++ src/main/java/ui/pages/bspb/BspbMainMenu.java
@@ -12,7 +12,7 @@ public class BspbMainMenu extends AbsBasePages {
         super(driver);
     }
 
-    @FindBy(id = "popover-trigger-:R6l6tcqbq6H1:")
+    @FindBy(css = "button[id='popover-trigger-:R6l6tcqbqmH1:']")
     public WebElement debitCards;
 
     @FindBy(xpath = "//*[contains(text(),'Партнеры и акции')]")
